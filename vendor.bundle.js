8b85a68e7bf55227e5fedd96d9627f8daf1d5ab4
BMorse09 <benmorse09@gmail.com>
2016-10-13 21:00:53 -0400

    trying to deploy front end

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(44);
+	module.exports = __webpack_require__(46);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 44:
+/***/ 46:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
